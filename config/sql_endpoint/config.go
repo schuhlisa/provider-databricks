4aef0965e1ba3eb672ed61a09dba0924d229f8e9
Geoffrey Lalanne <geoffrey.lalanne@teck.com>
2024-11-08 16:04:58 -0800

    Fix: cannot get jdbcUrl attribute

--- config/sql_endpoint/config.go
+++ config/sql_endpoint/config.go
@@ -7,14 +7,18 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/glalanne/provider-databricks/config/common"
-
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 )
 
 func sqlEndpointConnectionDetails(attr map[string]interface{}) (map[string][]byte, error) {
-	jdbcUrl, ok := attr["jdbcUrl"].(string)
-	if !ok {
-		return nil, errors.New("cannot get jdbcUrl attribute")
+
+	jdbcUrl, err := common.GetField(attr, "jdbcUrl")
+	if err != nil {
+		return nil, err
+	}
+
+	jdbcUrlBytes, err := base64.StdEncoding.DecodeString(jdbcUrl)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot serialize jdbcUrl key data")
 	}
 
 	hostname, err := common.GetField(attr, "odbcParams[0].hostname")
@@ -27,14 +31,26 @@ func sqlEndpointConnectionDetails(attr map[string]interface{}) (map[string][]byt
 		return nil, errors.Wrapf(err, "cannot serialize hostname key data")
 	}
 
+	path, err := common.GetField(attr, "odbcParams[0].path")
+	if err != nil {
+		return nil, err
+	}
+
+	pathBytes, err := base64.StdEncoding.DecodeString(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot serialize path key data")
+	}
+
 	// conn["odbcParams.hostname"] = []byte(a[0]["hostname"])
 	// conn["odbcParams.path"] = []byte(a[0]["path"])
 	// conn["odbcParams.port"] = []byte(a[0]["port"])
 	// conn["odbcParams.protocol"] = []byte(a[0]["protocol"])
 
 	conn := map[string][]byte{
-		xpv1.ResourceCredentialsSecretEndpointKey: []byte(jdbcUrl),
+		// xpv1.ResourceCredentialsSecretEndpointKey: []byte(jdbcUrl),
 		"hostname": hostnameBytes,
+		"path":     pathBytes,
+		"jdbcUrl":  jdbcUrlBytes,
 	}
 
 	return conn, nil
